ff02b1894f5c3917fd0e8f66d79cdb7ce333aa85
Shubham Samarth <shubhams@heaptrace.net>
2024-05-11 12:54:33 +0530

    feat - Messages

---
+++ Messages.java
@@ -0,0 +1,7 @@
+public class Messages {
+    public static void main(String args[]) {
+        System.out.println("First Line of message");
+        System.out.println("Second Line of message");
+        System.out.println("Third Line of message");
+    }
+}
